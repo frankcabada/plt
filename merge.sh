a4e26ff315fc6ac6ec7c6798927bbbd2bb62afd1
frankcabada <frankcabada@yahoo.com>
2016-10-18 22:24:38 -0400

    we got this

--- merge.sh
+++ merge.sh
@@ -1,5 +1,6 @@
 #Merge to master if build succeeds
+git fetch || exit
 git checkout master || exit
-git merge "$TRAVIS_COMMIT" || exit
+git merge "$TRAVIS_BRANCH" || exit #may need to change back to $TRAVIS_COMMIT
 git push
 

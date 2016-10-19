23471d37587e4585971cff60c1092d567349cd86
frankcabada <frankcabada@yahoo.com>
2016-10-18 22:45:16 -0400

    Ughhhh

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
 #Merge to master if build succeeds
+git fetch
 git checkout -b master --track origin/master || exit
 git merge "$TRAVIS_BRANCH" || exit #may need to change back to $TRAVIS_COMMIT
 git push

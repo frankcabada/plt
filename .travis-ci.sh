78003ccd239bd5d230e4bfd3025bf846c05afc01
meberko <m.e.berkowitz@gmail.com>
2016-10-23 16:17:20 -0400

    changing travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,8 +1,9 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	echo "NEVER PUSH ON MASTER!!!"
-	exit 1; 
+	exit 1;
 fi
 
+git update-ref HEAD master || exit
 git checkout master || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push

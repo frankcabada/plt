f7489603bb2cce046dc5105c9d777b7db0f5611b
meberko <m.e.berkowitz@gmail.com>
2016-11-01 17:03:07 -0400

    adding parserizer.ml

--- test/parser/parserize.ml
+++ test/parser/parserize.ml
@@ -1,50 +1,96 @@
-open Nullparser
+open Ast
+open Printf
 
-let stringify = function
-  (* Punctuation *)
-  | LPAREN -> "LPAREN"  | RPAREN -> "RPAREN"
-  | LBRACE -> "LBRACE"  | RBRACE -> "RBRACE"
-  | COMMA -> "COMMA"
+(* Unary operators *)
+let txt_of_unop = function
+  | Not -> "Not"
+  | Sub -> "Sub"
 
-  (* Arithmetic Operators *)
-  | PLUS -> "PLUS"     | MINUS -> "MINUS"
-  | TIMES -> "TIMES"   | DIVIDE -> "DIVIDE"
+(* Binary operators *)
+let txt_of_binop = function
+  (* Dist *)
+  | D_Plus -> "D_Plus"
+  | D_Times -> "D_Times"
+  | D_Shift -> "D_Shift"
+  | D_Stretch -> "D_Stretch"
+  | D_Power -> "D_Power"
+  | D_Sample -> "D_Sample"
+  (* Arithmetic *)
+  | Add -> "Add"
+  | Sub -> "Sub"
+  | Mult -> "Mult"
+  | Div -> "Div"
+  | Mod -> "Mod"
+  | Pow -> "Pow"
+  (* Boolean *)
+  | Or -> "Or"
+  | And -> "And"
+  | Eq -> "Eq"
+  | Neq -> "Neq"
+  | Less -> "Less"
+  | Leq -> "Leq"
+  | Greater -> "Greater"
+  | Geq -> "Geq"
+  | Cons -> "::"
 
-  (* Relational Operators *)
-  | EQ -> "EQ"    | NEQ -> "NEQ"
-  | LEQ -> "LEQ"  | GEQ -> "GEQ"
+(* Expressions *)
+let txt_of_num = function
+  | Num_int(x) -> string_of_int x
+  | Num_float(x) -> string_of_float x
 
-  (* Logical Operators & Keywords *)
-  | AND -> "AND"   | OR -> "OR"
-  | NOT -> "NOT"
+let rec txt_of_expr = function
+  | Num_lit(x) -> sprintf "Num_lit(%s)" (txt_of_num x)
+  | String_lit(x) -> sprintf "String_lit(%s)" x
+  | Bool_lit(x) -> sprintf "Bool_lit(%s)" (string_of_bool x)
+  | Void_lit -> "Void_lit"
+  | Id(x) -> sprintf "Id(%s)" x
+  | Unop(op, e) -> sprintf "Unop(%s, %s)" (txt_of_unop op) (txt_of_expr e)
+  | Binop(e1, op, e2) -> sprintf "Binop(%s, %s, %s)"
+      (txt_of_expr e1) (txt_of_binop op) (txt_of_expr e2)
+  | Call(f, args) -> sprintf "Call(%s, [%s])"
+      (txt_of_expr f) (txt_of_list args)
+  | Assign(x, e) -> sprintf "Assign(%s, %s)" x (txt_of_expr e)
+  | LDecl(l) -> sprintf "LDecl([%s])" (txt_of_list l)
+  | Dist(d) -> txt_of_dist d
+  | Discr_dist(d) -> txt_of_discr_dist d
+  | Fdecl(f)-> txt_of_fdecl f
+  | Cake(fdecl, args) -> sprintf "Cake(%s, [%s])"
+      (txt_of_expr fdecl) (txt_of_list args) 
+  | If(e1, e2, e3) -> sprintf "If(%s, %s, %s)"
+      (txt_of_expr e1) (txt_of_expr e2) (txt_of_expr e3)
+  
+and txt_of_dist d =
+  sprintf "Dist({ min=%s ; max=%s ; dist_func=%s })"
+    (txt_of_expr d.min) (txt_of_expr d.max) (txt_of_expr d.dist_func)
 
-  (* Assignment Operator *)
-  | ASSIGN -> "ASN"
+and txt_of_discr_dist d =
+  sprintf "Dist({ vals=%s ; weights=%s })"
+    (txt_of_expr d.vals) (txt_of_expr d.weights)
 
-  (* Conditional Operators *)
-  | IF -> "IF"
-  | ELSE -> "ELSE"
-  | ELSIF -> "ELSIF"
+(* Function declarations *)
+and txt_of_fdecl f =
+  sprintf "Fdecl({ params=[%s] ; body=%s ; return = %s })"
+    (String.concat " ; " f.params) (txt_of_stmts f.body) (txt_of_expr f.return)
 
-  (* End-of-File *)
-  | EOF -> "EOF"
+(* Lists *)
+and txt_of_list = function
+  | [] -> ""
+  | [x] -> txt_of_expr x
+  | _ as l -> String.concat " ; " (List.map txt_of_expr l)
 
-  (* Identifiers *)
-  | ID(string) -> "ID"
+(* Statements *)
+and txt_of_stmt = function
+  | Do(expr) -> sprintf "Do(%s)" (txt_of_expr expr)
 
-  | SEMI -> "SEMI"  | LBRACKET -> "LBRACKET" | RBRACKET -> "RBRACKET"
-  | LT -> "LT" | GT -> "GT" | INC -> "INC" | DEC -> "DEC"
-  | COLON -> "COLON" | FOR -> "FOR" | WHILE -> "WHILE" | BREAK -> "BREAK"
-  | RETURN -> "RETURN" | MAIN -> "MAIN"
-  | TRUE -> "TRUE" | FALSE -> "FALSE"
-  | INT -> "INT" | BOOL -> "BOOL" | VOID -> "VOID" | DOUBLE -> "DOUBLE"
-  | STRING -> "STRING" |NULL -> "NULL" | LITERAL(int) -> "LITERAL"
+and txt_of_stmts stmts =
+  let rec aux acc = function
+      | [] -> sprintf "[%s]" (String.concat " ; " (List.rev acc))
+      | stmt :: tl -> aux (txt_of_stmt stmt :: acc) tl
+  in aux [] stmts
 
+(* Program entry point *)
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  let rec print_tokens = function
-    | EOF -> " "
-    | token ->
-      print_endline (stringify token);
-      print_tokens (Scanner.token lexbuf) in
-  print_tokens (Scanner.token lexbuf)
+  let program = Parser.program Scanner.token lexbuf in
+  let result = txt_of_stmts program in
+  print_endline result

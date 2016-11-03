f5117751cbc934125c4e8a08118a5274fe2c386e
frankcabada <frankcabada@yahoo.com>
2016-11-03 17:33:03 -0400

    Color coded pass and fail tests

--- test/scanner/scripts/test.sh
+++ test/scanner/scripts/test.sh
@@ -1,10 +1,12 @@
 cat _base_scanner.test | ./tokenize > _base_scanner.res
 diff _base_scanner.out _base_scanner.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|      SCANNER: FIRST TEST PASSED       |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|      SCANNER: FIRST TEST FAILED       |"
   echo "-----------------------------------------"
@@ -13,10 +15,12 @@ fi
 cat _delimiters.test | ./tokenize > _delimiters.res
 diff _delimiters.out _delimiters.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|   SCANNER: DELIMITERS TEST PASSED     |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|   SCANNER: DELIMITERS TEST FAILED     |"
   echo "-----------------------------------------"
@@ -25,10 +29,12 @@ fi
 cat _control_flow.test | ./tokenize > _control_flow.res
 diff _control_flow.out _control_flow.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|  SCANNER: CONTROL FLOW TEST PASSED    |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|  SCANNER: CONTROL FLOW TEST FAILED    |"
   echo "-----------------------------------------"
@@ -37,10 +43,12 @@ fi
 cat _conditionals.test | ./tokenize > _conditionals.res
 diff _conditionals.out _conditionals.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|  SCANNER: CONDITIONALS TEST PASSED    |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|  SCANNER: CONDITIONALS TEST FAILED    |"
   echo "-----------------------------------------"
@@ -49,10 +57,12 @@ fi
 cat _arithmetic.test | ./tokenize > _arithmetic.res
 diff _arithmetic.out _arithmetic.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|   SCANNER: ARITHMETIC TEST PASSED     |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|   SCANNER: ARITHMETIC TEST FAILED     |"
   echo "-----------------------------------------"
@@ -61,10 +71,12 @@ fi
 cat _types.test | ./tokenize > _types.res
 diff _types.out _types.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|      SCANNER: TYPES TEST PASSED       |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|      SCANNER: TYPES TEST FAILED       |"
   echo "-----------------------------------------"
@@ -73,10 +85,12 @@ fi
 cat _misc.test | ./tokenize > _misc.res
 diff _misc.out _misc.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "-----------------------------------------"
   echo "|  SCANNER: MISCELLANEOUS TEST PASSED   |"
   echo "-----------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "-----------------------------------------"
   echo "|  SCANNER: MISCELLANEOUS TEST FAILED   |"
   echo "-----------------------------------------"

26121fbc3e61b7ab8a960e8fdcbf1872136a51e7
frankcabada <frankcabada@yahoo.com>
2016-11-03 17:35:59 -0400

    Implemented test color code for parser test script

--- test/parser/scripts/test.sh
+++ test/parser/scripts/test.sh
@@ -1,11 +1,13 @@
 cat base_scanner_test.txt | ./tokenize > base_scanner_test.res
 diff base_scanner_test.out base_scanner_test.res > /dev/null
 if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
   echo "---------------------------------------"
   echo "|      FIRST SCANNER TEST PASSED      |"
   echo "---------------------------------------"
 else
+  echo -e "\e[0;31m"
   echo "---------------------------------------"
   echo "|      FIRST SCANNER TEST FAILED      |"
-  echo "--------------------------------------"
+  echo "---------------------------------------"
 fi

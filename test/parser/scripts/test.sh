26705d886c06114edec6aeefb8640c9d1fa5cc4d
meberko <m.e.berkowitz@gmail.com>
2016-11-09 14:10:41 -0500

    adding parser tests for assign, formal opts, primitive decls, and statements

--- test/parser/scripts/test.sh
+++ test/parser/scripts/test.sh
@@ -41,3 +41,59 @@ else
   echo "|  PARSER: MAIN WITH FDECL TEST FAILED  |"
   echo "-----------------------------------------"
 fi
+
+cat _main_with_assign.test | menhir --interpret --interpret-show-cst parser.mly > _main_with_assign.res
+diff _main_with_assign.out _main_with_assign.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| PARSER: MAIN WITH ASSIGN TEST PASSED  |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| PARSER: MAIN WITH ASSIGN TEST FAILED  |"
+  echo "-----------------------------------------"
+fi
+
+cat _primitive_decls.test | menhir --interpret --interpret-show-cst parser.mly > _primitive_decls.res
+diff _primitive_decls.out _primitive_decls.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| PARSER: PRIMITIVE VDECLS TEST PASSED  |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| PARSER: PRIMITIVE VDECLS TEST FAILED  |"
+  echo "-----------------------------------------"
+fi
+
+cat _formal_opts.test | menhir --interpret --interpret-show-cst parser.mly > _formal_opts.res
+diff _formal_opts.out _formal_opts.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|    PARSER: FORMAL OPTS TEST PASSED    |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|    PARSER: FORMAL OPTS TEST FAILED    |"
+  echo "-----------------------------------------"
+fi
+
+cat _stmts.test | menhir --interpret --interpret-show-cst parser.mly > _stmts.res
+diff _stmts.out _stmts.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|    PARSER: STATEMENTS TEST PASSED     |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|    PARSER: STATEMENTS TEST FAILED     |"
+  echo "-----------------------------------------"
+fi

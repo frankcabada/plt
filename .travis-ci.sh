39786e269f7050e4e79fb8fb2907193c52399381
meberko <m.e.berkowitz@gmail.com>
2016-11-21 12:53:55 -0500

    okay I've said this before but this SHOULD work

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ sudo apt-get install m4 llvm software-properties-common
 sudo add-apt-repository --yes ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq -y ocaml ocaml-native-compilers menhir opam
-opam init
+opam init -a
 eval `opam config env`
 opam install llvm.3.4 ocamlfind
 
@@ -19,11 +19,14 @@ cd ../parser
 ./scripts/test.sh
 ./scripts/clean.sh
 echo ""
-echo "Testing Hello World!"
+echo "-----------------------------------------"
+echo "|           Testing Hello World!        |"
+echo "-----------------------------------------"
 echo ""
 cd ../../hello_world
 ./scripts/build.sh > build.log
 cat hello_world.cmat | ./cmat.native -c
 lli hello_world.ll
+echo ""
 echo ""
 exit 0

544b1d15ab8afcb8ea73b2ef4739f5b0f974377f
meberko <m.e.berkowitz@gmail.com>
2016-11-21 12:25:45 -0500

    trying to add hello world to .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,7 @@
 export PATH="/usr/bin:$PATH"
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers menhir
+sudo apt-get install -qq -y ocaml ocaml-native-compilers menhir m4 llvm opam
+opam install llvm.3.6 ocamlfind
 
 echo "OCaml version: "
 ocaml -version
@@ -12,4 +13,12 @@ cd ./test/scanner
 cd ../parser
 ./scripts/test.sh
 ./scripts/clean.sh
+echo ""
+echo "Testing Hello World!"
+echo ""
+cd ../../hello_world
+./scripts/build.sh > build.log
+cat hello_world.cmat | ./cmat.native -c
+lli hello_world.ll
+echo ""
 exit 0

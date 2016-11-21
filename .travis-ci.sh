11d13add1e45e832a504ac1fea6ac8fc178a7b71
meberko <m.e.berkowitz@gmail.com>
2016-11-21 12:38:25 -0500

    sigh, maybe this will work

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,6 +4,8 @@ sudo apt-get install m4 llvm software-properties-common
 sudo add-apt-repository --yes ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq -y ocaml ocaml-native-compilers menhir opam
+opam init
+eval `opam config env`
 opam install llvm.3.4 ocamlfind
 
 echo "OCaml version: "

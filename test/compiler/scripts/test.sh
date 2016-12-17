f86fad1ce7b94e2472329bb9836a5c5113b079d8
frankcabada <frankcabada@yahoo.com>
2016-12-17 16:44:30 -0500

    New fail tests for compiler

--- test/compiler/scripts/test.sh
+++ test/compiler/scripts/test.sh
@@ -104,3 +104,185 @@ else
   echo "| COMPILER: MATRIX LITERAL TEST FAILED  |"
   echo "-----------------------------------------"
 fi
+
+./cmat.native -c fail/_illegal_float_equality.test fail/_illegal_float_equality.ll >& fail/_illegal_float_equality.res
+diff fail/_illegal_float_equality.out fail/_illegal_float_equality.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: FLOAT EQUALITY TEST PASSED  |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: FLOAT EQUALITY TEST FAILED  |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_logical_operators.test fail/_logical_operators.ll >& fail/_logical_operators.res
+diff fail/_logical_operators.out fail/_logical_operators.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|COMPILER: LOGICAL OPERATORS TEST PASSED|"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|COMPILER: LOGICAL OPERATORS TEST FAILED|"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_matrix_equality.test fail/_matrix_equality.ll >& fail/_matrix_equality.res
+diff fail/_matrix_equality.out fail/_matrix_equality.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MATRIX EQUALITY TEST PASSED |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MATRIX EQUALITY TEST FAILED |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_duplicate_global.test fail/_duplicate_global.ll >& fail/_duplicate_global.res
+diff fail/_duplicate_global.out fail/_duplicate_global.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: DUPLICATE GLOBAL TEST PASSED |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: DUPLICATE GLOBAL TEST FAILED |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_function_nonexistent.test fail/_function_nonexistent.ll >& fail/_function_nonexistent.res
+diff fail/_function_nonexistent.out fail/_function_nonexistent.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER:MISSING FUNCTION TEST PASSED |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER:MISSING FUNCTION TEST FAILED |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_incorrect_function_args.test fail/_incorrect_function_args.ll >& fail/_incorrect_function_args.res
+diff fail/_incorrect_function_args.out fail/_incorrect_function_args.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|  COMPILER: FUNCTION ARGS TEST PASSED  |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|  COMPILER: FUNCTION ARGS TEST FAILED  |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_matrix_mismatch_add.test fail/_matrix_mismatch_add.ll >& fail/_matrix_mismatch_add.res
+diff fail/_matrix_mismatch_add.out fail/_matrix_mismatch_add.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MATRIX MISMATCH TEST PASSED |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MATRIX MISMATCH TEST FAILED |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_no_return.test fail/_no_return.ll >& fail/_no_return.res
+diff fail/_no_return.out fail/_no_return.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MISSING RETURN TEST PASSED  |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: MISSING RETURN TEST FAILED  |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_return_mismatch.test fail/_return_mismatch.ll >& fail/_return_mismatch.res
+diff fail/_return_mismatch.out fail/_return_mismatch.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: RETURN MISMATCH TEST PASSED |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "| COMPILER: RETURN MISMATCH TEST FAILED |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_void_formal.test fail/_void_formal.ll >& fail/_void_formal.res
+diff fail/_void_formal.out fail/_void_formal.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID FORMAL TEST PASSED   |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID FORMAL TEST FAILED   |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_void_global.test fail/_void_global.ll >& fail/_void_global.res
+diff fail/_void_global.out fail/_void_global.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID GLOBAL TEST PASSED   |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID GLOBAL TEST FAILED   |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_void_local.test fail/_void_local.ll >& fail/_void_local.res
+diff fail/_void_local.out fail/_void_local.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID LOCAL TEST PASSED    |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID LOCAL TEST FAILED    |"
+  echo "-----------------------------------------"
+fi
+
+./cmat.native -c fail/_void_return.test fail/_void_return.ll >& fail/_void_return.res
+diff fail/_void_return.out fail/_void_return.res > /dev/null
+if [ $? = 0 ]; then
+  echo -e "\e[0;32m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID RETURN TEST PASSED   |"
+  echo "-----------------------------------------"
+else
+  echo -e "\e[0;31m"
+  echo "-----------------------------------------"
+  echo "|   COMPILER: VOID RETURN TEST FAILED   |"
+  echo "-----------------------------------------"
+fi
